4a0d57d910248d33bf4aba1f5bf4b25868e7c244
daiqun <dq2619101152@foxmail.com>
2019-04-23 17:13:44 +0800

    add swagger2

--- src/main/java/com/spring/hero/controller/HelloController.java
+++ src/main/java/com/spring/hero/controller/HelloController.java
@@ -3,6 +3,9 @@ package com.spring.hero.controller;
 import com.alibaba.fastjson.JSONObject;
 import com.spring.hero.mapper.Dept;
 import com.spring.hero.service.DeptService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,16 +20,21 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/home")
+@Api("HelloController相关的api")
 public class HelloController {
 
     @Autowired
     private DeptService deptService;
 
+    @ApiOperation(value = "测试get请求", notes = "测试")
+    @ApiImplicitParam(name = "sayHello", value = "测试", dataType = "String", paramType = "path")
     @GetMapping(value = "/hello/{name}")
     public String sayHello(@PathVariable("name") String name) {
         return name;
     }
 
+    @ApiOperation(value = "测试get请求", notes = "注意事项")
+    @ApiImplicitParam(name = "findDept", value = "查询员工", dataType = "String", paramType = "path")
     @GetMapping("/dept")
     public String findDept() {
         List<Dept> list = deptService.list();
--- src/main/java/com/spring/hero/controller/UserController.java
+++ src/main/java/com/spring/hero/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.spring.hero.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
@@ -11,9 +13,12 @@ import java.security.Principal;
  * @version 2019.04.23
  */
 @RestController
+@Api("UserController相关的api")
 public class UserController {
 
-    @RequestMapping(value = "/user")
+    @ApiOperation(value = "测试登陆请求", notes = "用户登陆")
+    @ApiImplicitParam(name = "login", value = "用户登陆", dataType = "String", paramType = "path")
+    @PostMapping(value = "/user")
     private Principal login(Principal user) {
         return user;
     }
