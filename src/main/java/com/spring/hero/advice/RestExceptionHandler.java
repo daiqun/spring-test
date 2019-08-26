bc6683872aaab070855758d4519904ed9f84ccb2
daiqun <dq2619101152@foxmail.com>
2019-08-26 11:21:31 +0800

    异常处理

---
+++ src/main/java/com/spring/hero/advice/RestExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.spring.hero.advice;
+
+import com.spring.hero.exception.BusinessException;
+import com.spring.hero.pojo.dto.Result;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author daiqun
+ * @version 2019.08.26
+ */
+@ControllerAdvice
+public class RestExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(RestExceptionHandler.class);
+
+    /**
+     * 统一异常处理
+     *
+     * @param e Controller中抛出的异常
+     * @return 异常处理后需要返回给前台的对象
+     */
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public Result handle(Exception e) {
+        logger.error("RESTful Integration调用异常: {}", e.getMessage());
+
+        // 具体的异常处理逻辑
+        if (e instanceof BusinessException) {
+
+        }
+
+        return new Result(Boolean.FALSE, e.getMessage());
+    }
+}
--- src/main/java/com/spring/hero/service/impl/UserServiceImpl.java
+++ src/main/java/com/spring/hero/service/impl/UserServiceImpl.java
@@ -8,7 +8,6 @@ import com.spring.hero.utils.EncodeUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.List;
 
 /**
  * @author daiqun
@@ -24,8 +23,8 @@ public class UserServiceImpl implements UserService {
     public User userLogin(String username, String password) {
         User user = userMapper.findByName(username);
         String pass = EncodeUtils.encodePassword(password);
-        if (!pass.equals(user.getPassword())) {
-            throw new BusinessException("用户名或密码错误");
+        if (user == null || !pass.equals(user.getPassword())) {
+            throw new BusinessException("用户名不存在或账号密码错误");
         }
         return user;
     }
